cecc80c0509518202b8fece7a794173c298967c9
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-02-14 22:22:14 +0000

    .

--- setup/app.js
+++ setup/app.js
@@ -1,1 +1,19 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["#bbff99", "#ff8566", "#6666ff", "#ffff33", "#b366ff"];//array
+
+const btn = document.getElementById("btn");
+
+const color = document.querySelector(".color");
+
+
+btn.addEventListener("click", function(){
+    try{
+        const colorRecup = colors[Math.floor(Math.random() * colors.length )];
+        color.textContent = colorRecup ;
+        document.body.style.background = colorRecup;
+    }catch(err){
+        console.log(err);
+    }finally{
+        console.log(document.body);
+    }
+  
+})

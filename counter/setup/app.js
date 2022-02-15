8d5b30b1f065a8eb0a7051e1f5672e260073b3d8
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-02-15 11:37:24 +0000

    Increase, Decrease

--- counter/setup/app.js
+++ counter/setup/app.js
@@ -7,7 +7,39 @@ const btnIncrease = document.getElementById("increase");
 const btnReset = document.getElementById("reset");
 
 let valeur = val.textContent;
+//Increase Button
 btnIncrease.addEventListener("click",function(){
     valeur ++;
-    console.log(valeur);
+    val.textContent = valeur;
+    colorChange();
 });
+
+//Decrease Buuton function
+btnDecrease.addEventListener("click",function(){
+    valeur -- ;
+    val.textContent = valeur;
+    colorChange();
+});
+
+
+//funtion to reset to 0
+
+btnReset.addEventListener("click",function(){
+    valeur = 0;
+    val.textContent = valeur;
+    colorChange();
+});
+
+
+
+//function to change color of value Number
+
+function colorChange (){
+    if(valeur>0)
+    val.style.color = 'green';
+    else if(valeur<0)
+    val.style.color='red';
+    else if( valeur === 0)
+    val.style.color='black';
+
+}

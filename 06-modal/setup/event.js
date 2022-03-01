e96d0a755769e8c097f6c8390c97f7ca7de056b9
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-03-01 13:39:15 +0200

    updates

---
+++ 06-modal/setup/event.js
@@ -0,0 +1,12 @@
+const box1 = document.querySelector("box-shadow");
+window.addEventListener("DOMContentLoaded", function(){
+    box1.display= "noone";
+}
+)
+
+const btn = document.querySelector(".bouton");
+const btn1 = document.querySelector(".bouton");
+
+btn .addEventListener("click", ()=> {
+    
+})

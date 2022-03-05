46b136b588dd7922facdb7b5bea1be765b37e893
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-03-05 11:31:53 +0200

    Update app.js

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -1,6 +1,8 @@
-const btnOpenModal = document.querySelector(".modal-btn");
+const btnOpenModal = document.querySelector(".modal-btn");/**open modal */
+const modal =document.querySelector(".modal-overlay");/**modal content */
+const closeBtn = document.querySelector(".close-btn");/**close Button */
 
 
 btnOpenModal.addEventListener("click", function(){
-console.log("hello world");
+
 })

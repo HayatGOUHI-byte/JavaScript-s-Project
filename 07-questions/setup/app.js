e8565d4cc8d695d0fa00971cfb3c5f50d25481fc
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-03-08 11:06:18 +0200

    Updating

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,23 @@
 //using selectors inside the element
 // traversing the dom
+
+
+const plusIcon  =document.querySelector(".plus-icon");
+const MinusIcon = document.querySelector(".minus-icon");
+const Reponse = document.querySelector(".respond");
+
+
+
+// window.addEventListener("DOMContentLoaded",function(){
+//     Response.classList.add(".not-visible");
+// })
+
+plusIcon.addEventListener("click", () => {
+  Response.classList.add('.visible');
+
+})
+
+
+// MinusIcon.addEventListener("click", () => {
+    
+// }) 

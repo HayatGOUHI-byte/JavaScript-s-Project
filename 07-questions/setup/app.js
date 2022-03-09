9bd491b2494c81b0ca42732006880c1e12ea0f24
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-03-09 11:38:34 +0200

    Updating Mesage

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,12 +1,20 @@
+/*******************autre methode de se faire************************ */
 
- const btns = document.querySelectorAll(".question-btn");
+const question = document.querySelector(".question");
+question.addEventListener("click", () => 
+{
+  var msg = "Bonjour tout le monde";
+  console.log( msg);
+})
+/************************* */
+//  const btns = document.querySelectorAll(".question-btn");
 
- btns.forEach(function(btn){
-        btn.addEventListener("click",function(e){
-        const question = e.currentTarget.parentElement.parentElement;
-        question.classList.toggle("show-text");
+//  btns.forEach(function(btn){
+//         btn.addEventListener("click",function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle("show-text");
 
-      })
- })
+//       })
+//  })
 
 
